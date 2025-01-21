8859adee445416b913eaca95722e00ba5587928d
Sam Wu <swu@nait.ca>
2025-01-20 21:00:10 -0700

    End of Jan 20 class

---
+++ dmit2015-firebase-faces-app/src/main/java/dmit2015/model/Student.java
@@ -0,0 +1,13 @@
+package dmit2015.model;
+
+import lombok.Data;
+
+@Data
+public class Student {
+
+    private String id;
+    private String firstName;
+    private String lastName;
+    private String courseSection;
+
+}
